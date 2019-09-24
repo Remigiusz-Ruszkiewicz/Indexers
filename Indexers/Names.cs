b7d86600480b3bd66c24e8c2f67d38f9525bd990
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-24 17:38:25 +0200

    Add project files.

---
+++ Indexers/Names.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class Names
+    {
+        List<string> nameList = new List<string>();
+        public void Add(string name)
+        {
+            nameList.Add(name);
+        }
+        public int Lenght
+        {
+            get
+            {
+                return nameList.Count;
+            }
+        }
+        public int this[string name]
+        {
+            get
+            {
+                for (int i = 0; i < nameList.Count; i++)
+                {
+                    if (nameList[i] == name)
+                    {
+                        return i + 1;
+                    }
+                }
+                return 0;
+            }
+            set
+            {
+                for (int i = 0; i < nameList.Count; i++)
+                {
+                    if (i == value)
+                    {
+                        nameList[i] = name;
+                        return;
+                    }
+                }
+                if (value > nameList.Count -1)
+                {
+                    for (int i = nameList.Count -1 ;i < value; i++)
+                    {
+                        nameList.Add(null);
+                    }
+                    nameList[value] = name;
+                    return;
+                }
+                nameList.Add(name);
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names names = new Names();
+            names.Add("Roman"); //1
+            names.Add("Anna"); //2
+            names.Add("Jarek"); //3
+            //int i = names["Anna"]; // i ==2
+            //names["Jarek"] = 1;
+            names["Damian"] = 9;
+            int x = names["Roman"];
+            int y = names["Anna"];
+            int z = names["Jarek"];
+            int a = names["Damian"];
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(names.Lenght);
+            Console.Read();
+
+        }
+    }
+}
